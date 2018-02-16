32b919c846b98d371fdea3578d9ff946cf21c53c
dcbird92 <dcbird92@gmail.com>
2018-02-16 03:39:45 -0700

    hopefully runs faster

--- Assignment_5/Rumor_Mill.py
+++ Assignment_5/Rumor_Mill.py
@@ -1,39 +1,46 @@
 def rumorReport(studentName, studentList, visit):
     dayList = []
     subDayList = []
-    dayList.append(studentName)
+    dayListAdd = dayList.append
+    subDayListAdd = subDayList.append
+    subDayListSort = subDayList.sort
+    subDayListPop = subDayList.pop
+    dayListAdd(studentName)
     visit[studentName] = 1
-    remove = 0;
-    tempremove = 0;
+    remove = 0
+    tempremove = 0
+    loners = False
     # add the initial list of friends
     for friends in studentList[studentName]:
-        subDayList.append(friends)
+        subDayListAdd(friends)
         visit[friends] = 1
         remove += 1
-    subDayList.sort()
+    subDayListSort()
     # add the next day of vertices
     while remove > 0:
-        name = subDayList.pop(0)
-        dayList.append(name)
+        name = subDayListPop(0)
+        dayListAdd(name)
         remove -= 1
         for toldRumor in studentList[name]:
             if visit[toldRumor] is 0:
-                subDayList.append(toldRumor)
+                subDayListAdd(toldRumor)
                 visit[toldRumor] = 1
                 tempremove += 1
         if remove is 0:
             remove = tempremove
-            tempremove = 0;
-            subDayList.sort()
+            tempremove = 0
+            subDayListSort()
     for student, wasVisited in visit.items():
         if wasVisited == 0:
-            subDayList.append(student)
-    if len(subDayList) > 0:
-        subDayList.sort()
+            subDayListAdd(student)
+            loners = True
+    if loners:
+        subDayListSort()
         for untold in subDayList:
-            dayList.append(untold)
+            dayListAdd(untold)
     for students in dayList:
         print(students, end=" ")
+
 
 def main():
     students = {}
